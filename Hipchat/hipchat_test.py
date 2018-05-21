e9631349f92d9aa119f4ed0fda3db3a11a56de25
solarez1 <rrahman@sdl.com>
2018-05-21 16:32:21 +0100

    Update hipchat_test.py

--- Hipchat/hipchat_test.py
+++ Hipchat/hipchat_test.py
@@ -3,6 +3,6 @@ import requests, json
 #c6cS2qXSv1zRyUUXpPsu3bebVF43wx8bvPQK5vg6
 
 amessage = 'Hello World!'
-room = 'https://api.hipchat.com/v2/room/LoggingTest/notification'
-headers = {'Authorization':'Bearer 8OaIJuU0ViOvav3SAsE504Q3XkVxpmPG8Zadq70y', 'Content-type':'application/json'}
+room = 'https://api.hipchat.com/v2/room/room_name/notification'
+headers = {'Authorization':'Bearer room_id', 'Content-type':'application/json'}
 requests.post(url = room, data = json.dumps({'message':amessage}), headers = headers)
