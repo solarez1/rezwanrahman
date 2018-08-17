29b398598633f528c0d1df471bea7280434a7eb2
solarez1 <rrahman@sdl.com>
2018-08-17 14:27:47 +0100

    Update zabbix_multi_item_delete.py

--- zabbix_multi_item_delete.py
+++ zabbix_multi_item_delete.py
@@ -8,6 +8,6 @@ from pprint import pprint
 items = {}
 searchterm = 'blahdeblah'
 for host in hosts:	
-	items[host] = (zabbix_get_item(hostname=host, username='rrahman', password='R32wan1234', searchterm=searchterm))
+	items[host] = (zabbix_get_item(hostname=host, username='rrahman', password='MyPassword', searchterm=searchterm))
 	del_item = (items[host][searchterm])
 	pprint(zabbix_delete_item(username='rrahman', password='R32wan1234', item=del_item))
